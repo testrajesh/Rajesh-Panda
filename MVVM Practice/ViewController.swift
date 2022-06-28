b09380b6b47b390658bd0e5ec686422f2dace1ea
Rajesh <rajesh.panda@melorra.com>
2022-06-28 20:18:32 +0530

    Initial Commit

---
+++ MVVM Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Practice
+//
+//  Created by Rajesh Panda on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
